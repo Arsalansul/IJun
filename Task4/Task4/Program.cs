0b0fe04c6b18516ea88aca8120202909387631f3
arsalansul <arsalansul@gmail.com>
2020-07-28 09:09:26 +0700

    task 4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Unit
+    {
+        public int Health { get; protected set; }
+        public int Armor { get; protected set; }
+
+        public Unit(int health, int armor)
+        {
+            Health = health;
+            Armor = armor;
+        }
+        public void TakeDamage(int damage)
+        {
+            ProcessDamage(damage);
+            if (Health <= 0)
+            {
+                Console.WriteLine("Я умер");
+            }
+        }
+
+        public virtual void ProcessDamage(int damage)
+        {
+            Health -= damage - Armor;
+        }
+    }
+    class Wombat : Unit
+    {
+        public Wombat(int health, int armor) : base(health, armor)
+        {
+        }
+    }
+
+    class Human : Unit
+    {
+        public int Agility { get; private set; }
+        public Human(int health, int armor, int agility) : base(health, armor)
+        {
+            Agility = agility;
+        }
+        public override void ProcessDamage(int damage)
+        {
+            Health -= damage / Agility;
+        }
+
+        
+    }
+}
